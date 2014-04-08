70d5ba377b4f2f88e54249dc9317504a1469c1ad
Mike-Dunton <Michael.Dunton@patlive.com>
2014-04-08 10:22:01 -0400

    changing access poitns

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var express = require('express'),
     goalsConstructor = require('./routes/goal'),
     goals = new goalsConstructor();
 
-app.get('/get/user/all', users.getAll);
-app.get('/get/user/:id', users.getById);
+app.get('/user/all', users.getAll);
+app.get('/user/:id', users.getById);
 
-app.get('/get/goal/user/:id', goals.getAllGoalsForUser);
-app.get('/get/goal/goal/:id', goals.getById);
+app.get('/goal/user/:id', goals.getAllGoalsForUser);
+app.get('/goal/:id', goals.getById);
 
 
 app.listen(3000);
